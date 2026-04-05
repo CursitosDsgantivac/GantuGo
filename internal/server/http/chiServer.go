81aef3c56331a5085f01ea4b41f38a137701f9c1
David Gantiva <dsgantivac@gmail.com>
2026-04-05 12:40:57 -0500

    adding html response

--- internal/server/http/chiServer.go
+++ internal/server/http/chiServer.go
@@ -2,9 +2,12 @@ package server
 
 import (
 	"fmt"
+	"html/template"
 	"io"
+	"log"
 	"main/internal/utils"
 	"net/http"
+	"os"
 
 	"github.com/go-chi/chi/v5"
 )
@@ -20,6 +23,60 @@ func CreateChiServer() {
 
 	r.Get("/test", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "Hello world from Chi server")
+	})
+
+	r.Get("/test/html", func(w http.ResponseWriter, r *http.Request) {
+		_, err := io.ReadAll(r.Body)
+		defer r.Body.Close()
+		// create the html template
+		templateString := `
+		<!DOCTYPE html>
+		<html>
+			<head>
+				<meta charset="UTF-8">
+				<title>{{.Title}}</title>
+			</head>
+			<body>
+				{{range .Items}}<div>{{ . }}</div>{{else}}<div><strong>no rows</strong></div>{{end}}
+			</body>
+		</html>`
+
+		check := func(err error) {
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+		// build the template
+		t, err := template.New("webpage").Parse(templateString)
+		check(err)
+
+		data := struct {
+			Title string
+			Items []string
+		}{
+			Title: "My page",
+			Items: []string{
+				"My photos",
+				"My blog",
+			},
+		}
+
+		// return the templated the first argument is the writer and the second argument is the data to be rendered
+		// the data is writed in the first argument
+		err = t.Execute(w, data)
+		check(err)
+
+		noItems := struct {
+			Title string
+			Items []string
+		}{
+			Title: "My another page",
+			Items: []string{},
+		}
+
+		err = t.Execute(os.Stdout, noItems)
+		check(err)
+
 	})
 
 	r.Put("/test/{id}", func(w http.ResponseWriter, r *http.Request) {
